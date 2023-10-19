65423944b87e50971a1dd7bd5f28a680a90fa443
idriss <idrissaamadouousmane@gmail.com>
2023-10-19 12:51:46 +0100

    Debut du projet

---
+++ index.js
@@ -0,0 +1,25 @@
+const main = document.querySelector("main");
+let arrayExercise = [
+    {pic: 0, min: 1},
+    {pic: 1, min: 1},
+    {pic: 2, min: 1},
+    {pic: 3, min: 1},
+    {pic: 4, min: 1},
+    {pic: 5, min: 1},
+    {pic: 6, min: 1},
+    {pic: 7, min: 1},
+    {pic: 8, min: 1},
+    {pic: 9, min: 1},
+]
+
+class Exercise {
+
+}
+
+const utils = {
+
+}
+
+const page = {
+    
+}
